e6f12074029562358687b334a62a3cc2e9065a3a
oraphaeldemelo <oraphaeldemelo@outlook.com>
2021-07-14 19:18:51 -0300

    Initial commit

---
+++ src/main/java/com/raphael/CadastroProdutos/CadastroProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.CadastroProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphael/CadastroProdutos/CadastroProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.CadastroProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
