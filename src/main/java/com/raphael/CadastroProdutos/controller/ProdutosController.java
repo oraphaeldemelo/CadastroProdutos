5f94736fabbbd2bd25eaee562a9d91315c4e593a
oraphaeldemelo <oraphaeldemelo@outlook.com>
2021-07-15 18:54:47 -0300

    Adding a remove endpoint

--- src/main/java/com/raphael/CadastroProdutos/controller/ProdutosController.java
+++ src/main/java/com/raphael/CadastroProdutos/controller/ProdutosController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,8 +30,8 @@ public class ProdutosController {
 	}
 	
 	@PostMapping
-	public Produto cadastrar(@RequestBody Produto novoProduto) {
-		return repository.save(novoProduto);
+	public ResponseEntity<Produto> cadastrar(@RequestBody Produto novoProduto) {
+		return ResponseEntity.ok(repository.save(novoProduto));
 		
 	}
 	
@@ -43,6 +44,12 @@ public class ProdutosController {
 		} else {
 			return ResponseEntity.notFound().build(); 
 		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		repository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 	
 
